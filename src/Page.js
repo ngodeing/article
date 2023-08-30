ec206707ba814abbd97a77789619a9abad7f9c2e
ngodeing <onlytaufik47@gmail.com>
2023-08-30 18:30:15 +0700

    Menambahkan halaman Page

---
+++ src/Page.js
@@ -0,0 +1,12 @@
+import './index.css';
+
+function Page() {
+  return (
+    <div className='m-5'>
+        <h1 className='font-bold text-3xl text-center mb-3'>Judul Artikel</h1>
+        <p className=' bg-slate-50 p-5 w-3/5 mx-auto my-5 rounded-lg'>isi artikel aowlw asdasd asdasd adssadasd sadasd asdasdasd asd asdasdasds adasd asdasdasd sasdsa</p>
+    </div>
+  )
+}
+
+export default Page;
