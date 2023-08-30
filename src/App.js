e31f58575b36924cf1b9cac891755fcce3949446
ngodeing <onlytaufik47@gmail.com>
2023-08-30 18:05:18 +0700

    Membuat Artikel menggunakan tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Article from './Article';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-slate-200 p-5 ">
+      <h1 className='font-bold text-3xl text-center mb-3'>Artikel Terbaru Hari Ini</h1>
+      <div className="flex justify-center align-middle flex-wrap">
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+      </div>
     </div>
   );
 }
---
+++ src/Article.js
@@ -0,0 +1,13 @@
+import './index.css';
+
+function Article() {
+  return (
+    <div className="Article w-96 bg-slate-50 p-6 rounded-lg flex-col m-5 shadow-xl transition transform hover:-translate-y-2
+    motion-reduce:transition-none motion-reduce:hover:transform-none cursor-pointer">
+      <h3 className='font-bold text-2xl mb-3'>Judul Artikel</h3>
+      <p>Isi Artikel awokoak aokoakook a aokoakoka okwoaoak wokoakow o awkokawkoa woa oawkoako woa  woao ao oaokaokaokkaok oa oao aooakoao </p>
+    </div>
+  );
+}
+
+export default Article;
